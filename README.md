53b50f9853eef2ded2120cb73ae58e9f0970c1c9
Jeffrey Becca <jeff.becca@protonmail.com>
2023-12-28 23:11:45 -0500

    sqlite db init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # katas
-collection of katas
+Collection of katas and some tools for managing them. While there will be a fair amount of programming puzzle type katas, I also plan on including many examples that are not puzzles, but instead daily programming tasks like simple file parsing, creating quick CLI tools and so on. 
+
+Extremely lofty goals include having a tool that generates input in which the katas can be tested against and proven as passed or failed, with some form of spaced repetition included. 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use lib_katas::test;
 mod cli;
-pub fn main() {
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
+use sqlx::ConnectOptions;
+use std::error::Error;
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + 'static>> {
     let args = cli::parse_cli_args();
     test();
+
+    let conn = SqliteConnectOptions::from_str("sqlite://data.db")?
+        .create_if_missing(true)
+        .journal_mode(SqliteJournalMode::Wal)
+        .read_only(false)
+        .connect()
+        .await?;
     println!("Hello, world!");
+    Ok(())
 }
