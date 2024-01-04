bfb4eff1b60c15614a17a9369d61e0703be1fdc7
Jeffrey Becca <jeff.becca@protonmail.com>
2024-01-03 21:02:30 -0500

    added lib
    implemented linear search and binary search. Also added rand
    crate in order to generate some tests of much larger size.
    
    Long run, I probably want to style this a bit like rustlings
    and have a bare branch that has only tests and no solutions.
    Run the tool to see what kata should be reviewed, implement,
    test, and update db with test result

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,56 @@
 mod db;
 
+use rand::prelude::*;
+
 pub fn test() {
     println!("Test");
     db::test();
 }
 
+fn linear_search(a: &[u32], num: u32) -> bool {
+    for element in 0..a.len() {
+        if a[element] == num {return true};
+    }
 
+    false
+}
+
+fn binary_search(a: &[u64], num: u64) -> bool {
+    let mut li = 0;
+    let mut hi = a.len();
+
+    while li < hi {
+        let mid = li + ((hi - li) / 2);
+        let val = a[mid];
+        if val == num {
+            return true
+        } else if val < num {
+            li = mid + 1
+        } else {
+            hi = mid
+        }
+    }
+
+    false
+}
+
+#[test]
+fn test_lin_search() {
+    assert_eq!(linear_search(&[1,2,3,4,5], 4), true);
+    assert_eq!(linear_search(&[1,2,3,4,5], 6), false);
+    assert_eq!(linear_search(&[1,2,3,4,5], 0), false);
+}
+
+
+#[test]
+fn test_bin_search() {
+    let mut rng = rand::thread_rng();
+
+    let mut vals: Vec<u64> = (0..100000).map(|_| rng.gen_range(0..2000000000)).collect::<Vec<u64>>();
+    vals.sort();
+    assert_eq!(binary_search(&[1,2,3,4,5], 4), true);
+    assert_eq!(binary_search(&[1,2,3,4,5], 6), false);
+    assert_eq!(binary_search(&[1,2,3,4,5], 0), false);
+    println!("Now the long test");
+    assert_eq!(binary_search(&vals[0..], 387902), vals.contains(&387902));
+}
